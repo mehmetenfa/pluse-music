78d5b6250bb2e21658bf10536ecfc4f4447109ab
qxan <mehmet.enfa@gmail.com>
2023-03-11 18:53:44 +0300

    header logo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from "react";
+import logo from '../assets/images/logo.png  '
 
 export const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
-  
+    <>
+      <header className="fixed top-0 left-0 z-50 w-screen h-[8vh] md:shadow-sm bg-white">
+        {/* desktop */}
+        <div className="hidden md:flex justify-between px-7 p-2">
+          <div className="logo flex">
+            <div>
+              <img src={logo} alt="logo" width="40px" height="40px" />
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
