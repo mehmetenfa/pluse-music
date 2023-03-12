bacb55280040aa1de8f92a57c13af3bf201b16c5
qxan <mehmet.enfa@gmail.com>
2023-03-12 12:29:09 +0300

    map

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import logo from '../assets/images/logo.png  '
+import logo from '../assets/images/logo.png'
 
 export const Header = () => {
+  const activeNavLink = ({ isActive }) => (isActive ? 'active' : 'NavLink')
   return (
     <>
       <header className="fixed top-0 left-0 z-50 w-screen h-[8vh] md:shadow-sm bg-white">
@@ -11,7 +12,18 @@ export const Header = () => {
             <div>
               <img src={logo} alt="logo" width="40px" height="40px" />
             </div>
+            <h2 className="text-2xl font-semibold ml-3">pluse</h2>
           </div>
+
+          {/* navlinks */}
+          <div className="menu">
+            <div className="flex">
+              {navBar.map((list,i) => (
+                <li className={`mx-5 py-2 ${activeNavLink}`} key={i}></li>
+              ))}
+            </div>
+          </div>
+          <div></div>
         </div>
       </header>
     </>
