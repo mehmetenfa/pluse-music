7280c07e6692fa0ad26deff25a9dca6323f846e5
qxan <mehmet.enfa@gmail.com>
2023-03-16 22:13:40 +0300

    sidebar.jsx ok

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -28,7 +28,19 @@ export const Sidebar = () => {
               <h1 className="text-md font-semibold">App Store</h1>
             </div>
           </div>
+          <div className="bg-secondary text-white flex center px-2 py-0.5 rounded-md">
+            <div className="">
+              <IoPlaySharp size={30} />
+            </div>
+            <div className="">
+              <span className="text-gray-300 text-[12px]">Download on the</span>
+              <h1 className="text-md font-semibold">Google Play</h1>
+            </div>
+          </div>
         </div>
+
+        <p className="text-sm mt-3">About Contact Legal Policy</p>
+        <span className="text-gray-500 text-[12px]">© Copyright 2022</span>
       </section>
     </>
   );
