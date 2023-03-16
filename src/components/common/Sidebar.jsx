ad6fb1dc41ad4e6f5d5c9a2cc8c6763ffc5ec52b
qxan <mehmet.enfa@gmail.com>
2023-03-16 22:04:18 +0300

    car_sm added sidebar.jsx

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from "react";
+import { AiFillApple } from "react-icons/ai";
+import { IoPlaySharp } from "react-icons/io5";
+import { recommand } from "../assets/data/data";
+import { Card_sm } from "./Card_sm";
 
 export const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
-}
+    <>
+      <section className="sidebar hero">
+        <h1 className="mb-5 text-lg font-semibold text-gray-600">5 Likes</h1>
+        {recommand.slice(0, 5).map((item, i) => (
+          <div className="mb-3">
+            <Card_sm cover={item.cover} name={item.name} tag={item.tag} i={i} />
+          </div>
+        ))}
+      </section>
+    </>
+  );
+};
