c6cf114ba6101e2852feda7ad989b9a05653eda1
qxan <mehmet.enfa@gmail.com>
2023-03-17 23:19:55 +0300

    layoutsidebar

--- src/components/layout/LayoutSidebar.jsx
+++ src/components/layout/LayoutSidebar.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Sidebar } from "../common/Sidebar";
 
-export const LayoutSidebar = () => {
+export const LayoutSidebar = ({ children }) => {
   return (
-    <div>LayoutSidebar</div>
-  )
-}
+    <>
+      <main className="md:flex md:justify-between px-5 mt-8">
+        <div className="content w-full md:w-[73%]">{children}</div>
+        <div
+          className="md:-[25%] border-2 border-solid border-gray-100 rounded-xl p-5"
+          style={{ width: sidebarWidth }}
+        >
+          <Sidebar />
+        </div>
+      </main>
+    </>
+  );
+};
