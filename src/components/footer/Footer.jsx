730cdca71be08375dc30ff397fe17bdca0d221c6
qxan <mehmet.enfa@gmail.com>
2023-03-17 22:55:54 +0300

    imports

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
-
+import img from '../assets/images/b4.jpg'
+import { AiOutlineHeart, AiOutlineMenu } from 'react-icons/ai'
+import { BiSkipNext, BiSkipPrevious } from 'react-icons/bi'
+import { BsPlayCircleFill } from 'react-icons/bs'
+import { MdRepeat } from 'react-icons/md'
+import { TbArrowsShuffle } from 'react-icons/tb'
+import { HiVolumeUp } from 'react-icons/hi'
+ 
 export const Footer = () => {
   return (
     <div>Footer</div>
